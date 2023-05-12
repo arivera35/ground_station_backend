93969569306c38fcdf5ee63452a58ecadb2b1dd8
Adriana Rivera <70164362+arivera35@users.noreply.github.com>
2023-05-12 12:47:00 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rotor.h"
+#include "serial.h"
+
+int main(){			
+	
+	printf("az rotor %d", az_rotor.baud_rate);
+	//int response = rot_init(az_rotor);
+	
+	//printf("response: %d\n", response);
+}
