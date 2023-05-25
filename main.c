042af67a4390bc717c8185e0b4a338065dd15c68
Adriana Rivera <arivera35@miners.utep.edu>
2023-05-25 10:34:35 -0700

    added tle and spg4 files

--- main.c
+++ main.c
@@ -7,13 +7,12 @@ int main(void){
   sROTOR_CONFIG az_rotor = {.baud_rate = 9600, .port_num = "/dev/ttyUSB1", .num_bits = 8};
   sROTOR_CONFIG el_rotor = {.baud_rate = 9600, .port_num = "/dev/ttyUSB0", .num_bits = 8};
 
-
-  int fd = serial_init(az_rotor.baud_rate, az_rotor.port_num, az_rotor.num_bits);
-  int wrsp = serial_write(fd, VERSION_ID);
-  printf("write response: %d\n", wrsp);
+  int fd = serial_init(el_rotor.baud_rate, el_rotor.port_num, el_rotor.num_bits);
+  serial_write(fd, STATUS);
   char response [255];
   int bytes_read = serial_read(fd, response, 255);
   printf("Num bytes read %d\n", bytes_read);
   printf("String received: %s\n", response);
   serial_close(fd);
+  
 }
--- rot_commands.h
+++ rot_commands.h
@@ -1,2 +1,10 @@
+#ifndef ROT_COMMANDS_H
+#define ROT_COMMANDS_H
+
 #define STATUS "Bin;"
 #define VERSION_ID "R1n;"
+#define STOP "Stn;"
+#define RESTORE_DEFAULT "W00;"
+#define READ_PRESETS "PRn;"
+
+#endif 
